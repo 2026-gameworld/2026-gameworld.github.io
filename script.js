11638861c64ce4a68793c25e4acfac673d2a9310
aminsh3mshadi-lgtm <amin.sh3mshadi@gmail.com>
2026-09-25 05:38:00 +0800

    Add files via upload

---
+++ script.js
@@ -0,0 +1,17 @@
+function playGame(gameName) {
+    if (gameName === "GameWorld Adventure") {
+        window.location.href = "game.html";
+    }
+}
+
+function comingSoon() {
+    alert("This game is coming soon!");
+}
+
+function showNews() {
+    alert("More GameWorld news is coming soon!");
+}
+
+function joinCommunity() {
+    alert("Community system coming soon!");
+}
